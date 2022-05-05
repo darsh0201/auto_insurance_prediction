b2664dfe7a5ad3b1e56229e602bc1d9eab67ff92
darsh0201 <81774272+darsh0201@users.noreply.github.com>
2022-05-05 15:30:30 +0530

    commit

--- proj-trial5-master/app.py
+++ proj-trial5-master/app.py
@@ -1,15 +1,50 @@
 import flask
 import pickle
 import pandas as pd
+import yaml
+from flask_mysqldb import MySQL
+
 
 # Use pickle to load in the pre-trained model
-with open(f'model/model6.pkl', 'rb') as f:
+with open(f'proj-trial5-master\model\model6.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Initialise the Flask app
 app = flask.Flask(__name__, template_folder='templates')
 
+# Configure db
+db = yaml.safe_load(open(
+    'C:/Users/Darsh/OneDrive/Desktop/auto_insurance_copy/proj-trial5-master/db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+
+mysql = MySQL(app)
+
 # Set up the main route
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if flask.request.method == 'POST':
+        # Fetch form data
+        userDetails = flask.request.form
+        name = userDetails['name']
+        email = userDetails['email']
+        phone = userDetails['phone']
+        subject = userDetails['subject']
+        comments = userDetails['comments']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO customers(name, email, phone, subject, comments) VALUES(%s, %s, %s, %s, %s)",
+                    (name, email, phone, subject, comments))
+        mysql.connection.commit()
+        cur.close()
+        return 'We will contact you soon'
+
+        # Just render the contact form, to get input
+    return(flask.render_template('contact.html'))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -66,4 +101,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
